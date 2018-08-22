fa25899ae59708838b1389f25e3cf7e6ec65212b
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-08-22 19:11:22 +0200

    use TransferLocalFile in BaseRuncard

--- analysis/tasks/BaseRuncard.py
+++ analysis/tasks/BaseRuncard.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import os
-
+from law.contrib.tasks import TransferLocalFile
 from analysis.framework import Task
 
-class BaseRuncard(Task):
+class BaseRuncard(TransferLocalFile, Task):
 
-  def output(self):
+  def single_output(self):
     return self.remote_target('{}.{}.run'.format(self.process, self.name))
-
-  def run(self):
-    self.output().parent.touch()
-    basename = os.path.basename(self.output().path)
-    analysis_path = os.environ['ANALYSIS_PATH']
-    input_path = '{}/{}'.format(analysis_path, basename)
-    with open(basename, 'r') as infile:
-      with self.output().open('w') as outfile:
-        outfile.write(infile.read())
 

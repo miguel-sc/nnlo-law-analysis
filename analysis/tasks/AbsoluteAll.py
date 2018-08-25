d09dd5877ed2ef50886dfce61c6cc0da7035fce9
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-08-25 20:57:30 +0200

    added AbsoluteAll

---
+++ analysis/tasks/AbsoluteAll.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import law
+import luigi
+import os
+
+from FnloCppreadFinal import FnloCppreadFinal
+
+from analysis.framework import Task
+
+class AbsoluteAll(Task, law.LocalWorkflow):
+
+  merge_dir = luigi.Parameter()
+  plots_dir = luigi.Parameter()
+  observables = luigi.ListParameter()
+
+  def create_branch_map(self):
+    branchmap = {}
+    i = 0
+    for observable in self.observables:
+      branchmap[i] = {
+        'observable': observable
+      }
+      i += 1
+    return branchmap
+
+  def workflow_requires(self):
+    return {
+      'fnlocppread': FnloCppreadFinal()
+    }
+
+  def output(self):
+    return law.LocalDirectoryTarget('{}/{}/AbsoluteAll/NNLO/{}'.format(self.plots_dir, self.name, self.branch_data['observable']))
+
+  def run(self):
+    self.output().touch()
+
+    datfile1 = '{}/{}/Combined/Final/{}.LO.{}.dat'.format(self.merge_dir, self.name, self.process, self.branch_data['observable'])
+    datfile2 = '{}/{}/Combined/Final/{}.NLO.{}.dat'.format(self.merge_dir, self.name, self.process, self.branch_data['observable'])
+    datfile3 = '{}/{}/Combined/Final/{}.NNLO.{}.dat'.format(self.merge_dir, self.name, self.process, self.branch_data['observable'])
+    logfile = '{}/{}/Combined/Final/{}.NNLO.{}.log'.format(self.merge_dir, self.name, self.process, self.branch_data['observable'])
+    outfile = '{}/{}.NNLO.{}'.format(self.output().path, self.process, self.branch_data['observable'])
+
+    os.system('fastnnlo_absolute_all_v2.py -d {} {} {} -l {} -o {}'.format(datfile1, datfile2, datfile3, logfile, outfile))
+

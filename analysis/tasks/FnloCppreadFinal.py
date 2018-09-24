8715f130eab11ca933133c95185c2b55e0607c5a
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2018-09-24 21:07:52 +0200

    MergeFinal and FnloCppreadFinal are now workflows

--- analysis/tasks/FnloCppreadFinal.py
+++ analysis/tasks/FnloCppreadFinal.py
@@ -9,29 +9,57 @@ from MergeFinal import MergeFinal
 
 from analysis.framework import Task
 
-class FnloCppreadFinal(Task):
+class FnloCppreadFinal(Task, law.LocalWorkflow):
 
   merge_dir = luigi.Parameter()
+  channels = luigi.Parameter()
+  final_tables = luigi.DictParameter()
+  observables = luigi.ListParameter()
+
   pdf = luigi.Parameter()
   scalecombs = luigi.Parameter()
   ascode = luigi.Parameter()
   norm = luigi.Parameter()
   scale = luigi.Parameter()
 
+  def create_branch_map(self):
+    branchmap = {}
+    i = 0
+    for observable in self.observables:
+      for channel in self.channels.split(' '):
+        branchmap[i] = {
+          'observable': observable,
+          'channel': channel
+        }
+        i += 1
+      for channel in self.final_tables:
+        branchmap[i] = {
+          'observable': observable,
+          'channel': channel
+        }
+        i += 1
+    return branchmap
+
+  def workflow_requires(self):
+    return {
+      'mergefinal': MergeFinal()
+    }
+
   def requires(self):
     return MergeFinal()
 
   def output(self):
-    return self.local_target('FnloCppreadFinal')
+    return law.LocalFileTarget('{}/{}/Combined/Final/{}.{}.{}.log'.format(self.merge_dir, self.name, self.process, self.branch_data['channel'], self.branch_data['observable']))
 
   def run(self):
-    for table in glob.glob(self.merge_dir + '/' + self.name + '/Combined/Final/*.tab.gz'):
-      parts = table.split('.')
-      parts.pop()
-      parts.pop()
-      logfile = '.'.join(parts)
+     
+    self.output().parent.touch()
 
-      os.system('fnlo-tk-cppread {} {} {} {} {} {} | tee {}.log'.format(table, self.pdf, self.scalecombs, self.ascode, self.norm, self.scale, logfile))
+    logfile = self.output().path
+    parts = logfile.split('.')
+    parts.pop()
+    parts.append('tab.gz')
+    table = '.'.join(parts)
 
-    self.output().touch()
+    os.system('fnlo-tk-cppread {} {} {} {} {} {} | tee {}'.format(table, self.pdf, self.scalecombs, self.ascode, self.norm, self.scale, logfile))
 
--- analysis/tasks/MergeFinal.py
+++ analysis/tasks/MergeFinal.py
@@ -9,31 +9,47 @@ from MergeFastProd import MergeFastProd
 
 from analysis.framework import Task
 
-class MergeFinal(Task):
+class MergeFinal(Task, law.LocalWorkflow):
 
   merge_dir = luigi.Parameter()
   final_tables = luigi.DictParameter()
   observables = luigi.ListParameter()
 
+  def create_branch_map(self):
+    i = 0
+    branchmap = {}
+    for order, channels in self.final_tables.iteritems():
+      for obs in self.observables:
+        branchmap[i] = {
+          'obs': obs,
+          'order': order,
+          'channels': channels
+        }
+        i += 1
+    return branchmap
+
+  def workflow_requires(self):
+    return {
+      'mergefastprod': MergeFastProd()
+    }
+
   def requires(self):
     return MergeFastProd()
 
   def output(self):
-    return self.local_target('MergeFinal')
+    return law.LocalFileTarget('{}/{}/Combined/Final/{}.{}.{}.tab.gz'.format(self.merge_dir, self.name, self.process, self.branch_data['order'], self.branch_data['obs']))
 
   def run(self):
 
-    prevdir = os.getcwd()
+    outfile = self.output().path
+    parts = outfile.split('.')
+    parts.pop()
+    parts.pop()
+    parts.append('merge2.log')
+    logfile = '.'.join(parts)
+    
+    tablestring = ''
+    for channel in self.branch_data['channels']:
+      tablestring += '{}/{}/Combined/Final/{}.{}.{}.tab.gz '.format(self.merge_dir, self.name, self.process, channel, self.branch_data['obs'])
+    os.system('fnlo-tk-merge2 -add {}{} | tee {}'.format(tablestring, outfile, logfile))
 
-    os.chdir('{}/{}/Combined/Final'.format(self.merge_dir, self.name))
-
-    for order, channels in self.final_tables.iteritems():
-      for obs in self.observables:
-        tablestring = ''
-        for channel in channels:
-          tablestring += '{}.{}.{}.tab.gz '.format(self.process, channel, obs)
-        os.system('fnlo-tk-merge2 -add {}{}.{}.{}.tab.gz | tee {}.{}.{}.merge2.log'.format(tablestring, self.process, order, obs, self.process, order, obs))
-
-    os.chdir(prevdir)
-
-    self.output().touch()
